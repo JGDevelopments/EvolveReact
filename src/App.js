9f47d83fe4493d977186fbbef97ba16d601b10cc
Julian Gaston <103215878+JGDevelopments@users.noreply.github.com>
2022-11-05 17:29:28 -0500

    Added Context, + and - buttons, more logic in ProductCard.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Store from './pages/Store'
 import Cancel from './pages/Cancel'
 import Success from './pages/Success'
+import {CartProvider} from "./CartContext";
 
 function App() {
     return (
 
-        <Container>
-            <NavbarComponenet></NavbarComponenet>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Store/>}/> {/*These are called pages */}
-                    <Route path="success" element={<Success/>}/>
-                    <Route path="cancel" element={<Cancel/>}/>
-                </Routes>
-            </BrowserRouter>
-        </Container>
+        <CartProvider>
+            <Container>
+                <NavbarComponenet></NavbarComponenet>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Store/>}/> {/*These are called pages */}
+                        <Route path="success" element={<Success/>}/>
+                        <Route path="cancel" element={<Cancel/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </Container>
+        </CartProvider>
     );
 }
 
