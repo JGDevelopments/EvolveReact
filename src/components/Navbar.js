da72f6525a94a6b064a2a155ffe4b19a03959291
Julian Gaston <103215878+JGDevelopments@users.noreply.github.com>
2022-11-06 17:57:15 -0600

    Added CartProduct component and added more information to cart modal.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,58 @@
 import React from 'react'
 import {Button, Container, Navbar, Modal, Nav} from "react-bootstrap"
-import {useState} from "react";
+import {useState, useContext} from "react";
+import {CartContext} from "../CartContext";
+import CartProduct from "./CartProduct";
 
 
 const NavbarComponenet = () => {
+    const cart = useContext(CartContext);
     const [show, setShow] = useState(false); //?
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
 
+    const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
+    const productTotal = cart.getTotalCost();
+
     return (
         <>
             <Navbar expand="sm">
-                <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
+                <Navbar.Brand href='/'>Evolve</Navbar.Brand>
                 <Navbar.Toggle/>
                 <Navbar.Collapse className='justify-content-end'>
-                    <Button onClick={handleShow}> Cart 0 Items </Button>
+                    { productsCount > 0 ?
+                        <Button onClick={handleShow}> Cart {productsCount} Items </Button>
+                        :
+                        <Button onClick={handleShow}> Add To Your Cart </Button>
+                    }
                 </Navbar.Collapse>
             </Navbar>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>
-                        Give Me Your Money!
+                        Cart Total:
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h1>Yea you heard it right :)</h1>
+                    {productsCount > 0 ?
+                        <>
+                            <p>Items in your cart:</p>
+                            {cart.items.map((currentProduct, idx) => (
+                                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+                            ))}
+
+                            <h1>Total ${cart.getTotalCost().toFixed(2 /* rounds the number */)}</h1>
+                            <Button variant={"success"} className={"my-2"}>Checkout</Button>
+                        </>
+
+                        :
+
+                        <h1>Nothing to see here.</h1>
+
+                    }
+
+
                 </Modal.Body>
             </Modal>
         </>
